065e0e1dd90cb9d915f3047996638ccc7e7b2fac
Kidscry <70219165+Kidscry@users.noreply.github.com>
2026-01-02 20:34:25 -0500

    Update sweethome.lua

--- games/sweethome.lua
+++ games/sweethome.lua
@@ -1,34 +1,27 @@
-local map = workspace:WaitForChild("Map")
-local parts = {}
+while true do
+    local map = workspace:WaitForChild("Map")
+    local parts = {}
 
-for _, part in pairs(map:GetChildren()) do
-    local prompt = part:FindFirstChild("Prompt")
-    if prompt and prompt:IsA("ProximityPrompt") and prompt.ActionText == "Collect" then
-        table.insert(parts, part)
+    for _, part in pairs(map:GetChildren()) do
+        local prompt = part:FindFirstChild("Prompt")
+        if prompt and prompt:IsA("ProximityPrompt") and prompt.ActionText == "Collect" then
+            table.insert(parts, part)
+        end
     end
-end
 
-local function firePrompt(prompt)
-    pcall(function()
-        prompt:InputHoldBegin()
-        wait(0.5)
-        prompt:InputHoldEnd()
-    end)
-end
-
-local function triggerPrompt()
-    if #parts > 0 then
-        local part = table.remove(parts, 1)
+    for _, part in pairs(parts) do
         local prompt = part:FindFirstChild("Prompt")
         local character = game.Players.LocalPlayer.Character
         if character and character:FindFirstChild("HumanoidRootPart") then
-            character:MoveTo(part.Position + Vector3.new(0, 5, 0))
-            wait(math.random(0.1, 0.15))
-            firePrompt(prompt)
-            wait(math.random(0.5, 0.6))
-            triggerPrompt()
+            pcall(function()
+                character:MoveTo(part.Position + Vector3.new(0, 5, 0))
+                wait(math.random(0.05, 0.15))
+                prompt:InputHoldBegin()
+                wait(0.65)
+                prompt:InputHoldEnd()
+                wait(math.random(0.3, 0.6))
+            end)
         end
+        wait(0.1)
     end
 end
-
-triggerPrompt()
